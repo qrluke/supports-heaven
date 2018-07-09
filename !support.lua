3b2f8d8482c178b135a45eb82424e91b5b4e3d51
Fyodor Kurlyuk <fyodor.kurlyuk@gmail.com>
2018-07-10 00:52:41 +0700

    Подгружаем текстуры шпор и выводим в окно - тесты

--- !support.lua
+++ !support.lua
@@ -1091,9 +1091,9 @@ var_main()
 mode = "Samp-Rp"
 
 --[[
-		МЕСТА, ДЛЯ РАСШИРЕНИЯ СКРИПТА, ИСКАТЬ ПО mode == ":
+		МЕСТА, ДЛЯ ДОБАВЛЕНИЯ ПОДДЕРЖКИ ДРУГИХ ПРОЕКТОВ, ИСКАТЬ ПО mode == ":
 		1. function main_init_supdoc() - инициализация картинок и метафайла для шпоры, у каждого проекта своя папка и свой метафайл.
-		2. function DEBUG_simulateSupport() - симуляция саппорта, на каждом проекте она разная.
+		2. function DEBUG_simulateSupport() - симуляция вопросов и чужих ответов, на каждом проекте она разная.
 		3. function DEBUG_simulateSupportAnswer() - симуляция ответа саппорта, на каждом проекте своё.
 		4. function RPC.onPlaySound() - false на звук отправки сообщения при включенном уведомлении со стороны скрипт. У каждого проекта может быть свой звук/не быть его вообще.
 		5. RPC.onServerMessage() - проверка афк в мессенджере смс, парсинг входящих и исходящих смсок (+ подмена цвета, false), парсинг вопросов, ответов и чужих ответов (+подмена цвета, false).
@@ -1111,6 +1111,8 @@ mode = "Samp-Rp"
 		17. imgui_messanger_sup_keyboard() -  способ отправки ответа.
 		18. imgui_messanger_sms_keyboard() - способ отправки смс.
 ]]
+
+
 -------------------------------------MAIN---------------------------------------
 function main()
   if not isSampfuncsLoaded() or not isSampLoaded() then return end
@@ -1316,7 +1318,17 @@ end
 
 function main_init_supdoc()
   if mode == "Samp-Rp" then
-    --test = imgui.CreateTextureFromFile(getWorkingDirectory().."\\test.png")
+    modpath = getGameDirectory().."\\moonloader\\resource\\sup\\samp-rp\\"
+    if doesDirectoryExist(modpath) and doesFileExist(modpath.."spur.txt") then
+      sampAddChatMessage("Загружаем текстуры шпор, это может занять время.", color)
+      spur = {}
+      for _ in io.lines(modpath.."spur.txt") do
+        local a, b = string.match(_, [["(.+)" = "(.+)"]])
+        local width, height = GetImageWidthHeight(modpath..a)
+        table.insert(spur, {img = imgui.CreateTextureFromFile(modpath..a), name = b, width = width, height = height})
+      end
+      print(inspect(spur))
+    end
   end
 end
 
@@ -1718,16 +1730,16 @@ function sup_ParseFastRespond_fr()
 end
 
 function sup_FastRespond_via_chat()
-    if cfg.supfuncs.fastrespondviachat then
-      if LASTID ~= -1 then
-        if sampIsPlayerConnected(LASTID) and sampGetPlayerNickname(LASTID) == LASTNICK then
-          sampSetChatInputEnabled(true)
-            if mode == "Samp-Rp" then sampSetChatInputText("/pm "..LASTID.." ") end
-        else
-          sampAddChatMessage("Ошибка: игрок, задавший вопрос, отключился.", color)
-        end
+  if cfg.supfuncs.fastrespondviachat then
+    if LASTID ~= -1 then
+      if sampIsPlayerConnected(LASTID) and sampGetPlayerNickname(LASTID) == LASTNICK then
+        sampSetChatInputEnabled(true)
+        if mode == "Samp-Rp" then sampSetChatInputText("/pm "..LASTID.." ") end
+      else
+        sampAddChatMessage("Ошибка: игрок, задавший вопрос, отключился.", color)
       end
     end
+  end
 end
 
 function sup_FastRespond_via_dialog_LASTID()
@@ -1737,32 +1749,32 @@ function sup_FastRespond_via_dialog_LASTID()
 end
 
 function sup_FastRespond_via_dialog(param, mod)
-    if cfg.supfuncs.fastrespondviadialog or mod then
-      if tonumber(param) ~= nil then
-        id = tonumber(param)
-        if sampIsPlayerConnected(id) then
-          if getfr == {} then
-            sampShowDialog(8320, "{D2D2D2}Ответ для "..sampGetPlayerNickname(id).."["..id.."]", "База быстрых ответов пуста.\nПополнить базу можно в настройках.", "Ясно.")
-          else
-            FRtext_D = "{FFD700}Вопрос{40E0D0}:{FF9D00} "..sup_getLastQuestion(sampGetPlayerNickname(id)).."\n\n"
-            for k, v in pairs(getfr) do
-              FRtext_D = FRtext_D..string.format("{FFD700}%s {40E0D0}- {FF9D00}%s", k, v).."\n"
-            end
-            sampShowDialog(8320, "{D2D2D2}Ответ для "..sampGetPlayerNickname(id).."["..id.."]", FRtext_D, "Выбрать", "", 1)
-            while sampIsDialogActive() do wait(0) end
-            local result, button, list, input = sampHasDialogRespond(8320)
-            if button == 1 then
-              FRnumber_D = sampGetCurrentDialogEditboxText(8320)
-              if tonumber(FRnumber_D) ~= nil and getfr[tonumber(FRnumber_D)] ~= nil then
-                  if mode == "Samp-Rp" then sampSendChat("/pm "..id.." "..getfr[tonumber(FRnumber_D)]) end
-              end
+  if cfg.supfuncs.fastrespondviadialog or mod then
+    if tonumber(param) ~= nil then
+      id = tonumber(param)
+      if sampIsPlayerConnected(id) then
+        if getfr == {} then
+          sampShowDialog(8320, "{D2D2D2}Ответ для "..sampGetPlayerNickname(id).."["..id.."]", "База быстрых ответов пуста.\nПополнить базу можно в настройках.", "Ясно.")
+        else
+          FRtext_D = "{FFD700}Вопрос{40E0D0}:{FF9D00} "..sup_getLastQuestion(sampGetPlayerNickname(id)).."\n\n"
+          for k, v in pairs(getfr) do
+            FRtext_D = FRtext_D..string.format("{FFD700}%s {40E0D0}- {FF9D00}%s", k, v).."\n"
+          end
+          sampShowDialog(8320, "{D2D2D2}Ответ для "..sampGetPlayerNickname(id).."["..id.."]", FRtext_D, "Выбрать", "", 1)
+          while sampIsDialogActive() do wait(0) end
+          local result, button, list, input = sampHasDialogRespond(8320)
+          if button == 1 then
+            FRnumber_D = sampGetCurrentDialogEditboxText(8320)
+            if tonumber(FRnumber_D) ~= nil and getfr[tonumber(FRnumber_D)] ~= nil then
+              if mode == "Samp-Rp" then sampSendChat("/pm "..id.." "..getfr[tonumber(FRnumber_D)]) end
             end
           end
-        else
-          sampAddChatMessage("Ошибка: игрок с заданным id оффлайн.", color)
         end
+      else
+        sampAddChatMessage("Ошибка: игрок с заданным id оффлайн.", color)
       end
     end
+  end
 end
 
 function sup_UnAnswered_via_samp_dialog()
@@ -1805,7 +1817,7 @@ function sup_UnAnswered_via_samp_dialog()
             UNANid = string.match(string.gsub(UNANtext, "{......}", ""), "%[(%d+)%]")
             UNANid = string.match(UNANtext, UNID.." - .+%[(%d+)%].+\n")
             if UNANid ~= nil and sampIsPlayerConnected(UNANid) then
-              if mode == "Samp-Rp" then  sampSendChat("/pm "..tonumber(UNANid).." "..UNANanswe) end
+              if mode == "Samp-Rp" then sampSendChat("/pm "..tonumber(UNANid).." "..UNANanswe) end
             end
           else
             if tonumber(UNANanswer) ~= nil then
@@ -1813,7 +1825,7 @@ function sup_UnAnswered_via_samp_dialog()
               UNANid = string.match(UNANtext, tonumber(UNANanswer).." - .+%[(%d+)%].+\n")
               if sampIsPlayerConnected(UNANid) then
                 sampSetChatInputEnabled(true)
-                if mode == "Samp-Rp" then  sampSetChatInputText("/pm "..tonumber(UNANanswer).." ") end
+                if mode == "Samp-Rp" then sampSetChatInputText("/pm "..tonumber(UNANanswer).." ") end
               end
             end
           end
@@ -1910,6 +1922,11 @@ function imgui.OnDrawFrame()
     end
     imgui.End()
   end
+  imgui.Begin("test window")
+  for k, v in pairs(spur) do
+    imgui.Image(v["img"], imgui.ImVec2(v["width"], v["height"]))
+  end
+  imgui.End()
 end
 
 function imgui_menu()
@@ -5246,6 +5263,197 @@ do
       end
     end
     return tables[1]
+  end
+  --[[
+Get Image Size
+By: MikuAuahDark
+Allows you to get image/video size from most files.
+Supported Image/Video Files(by priority order):
+1. Portable Network Graphics
+2. Windows Bitmap
+3. JPEG
+4. GIF
+5. Photoshop Document
+6. Truevision TGA
+7. JPEG XR/TIFF*
+8. MP4
+9. AVI
+(*) = experimental
+]]
+
+  function GetImageWidthHeight(file)
+    local fileinfo = type(file)
+    if type(file) == "string" then
+      file = assert(io.open(file, "rb"))
+    else
+      fileinfo = file:seek("cur")
+    end
+    local function refresh()
+      if type(fileinfo) == "number" then
+        file:seek("set", fileinfo)
+      else
+        file:close()
+      end
+    end
+    local width, height = 0, 0
+    file:seek("set", 1)
+    -- Detect if PNG
+    if file:read(3) == "PNG" then
+      --[[
+			The strategy is
+			1. Seek to position 0x10
+			2. Get value in big-endian order
+		]]
+      file:seek("set", 16)
+      local widthstr, heightstr = file:read(4), file:read(4)
+      if type(fileinfo) == "number" then
+        file:seek("set", fileinfo)
+      else
+        file:close()
+      end
+      width = widthstr:sub(1, 1):byte() * 16777216 + widthstr:sub(2, 2):byte() * 65536 + widthstr:sub(3, 3):byte() * 256 + widthstr:sub(4, 4):byte()
+      height = heightstr:sub(1, 1):byte() * 16777216 + heightstr:sub(2, 2):byte() * 65536 + heightstr:sub(3, 3):byte() * 256 + heightstr:sub(4, 4):byte()
+      return width, height
+    end
+    file:seek("set")
+    -- Detect if BMP
+    if file:read(2) == "BM" then
+      --[[
+			The strategy is:
+			1. Seek to position 0x12
+			2. Get value in little-endian order
+		]]
+      file:seek("set", 18)
+      local widthstr, heightstr = file:read(4), file:read(4)
+      refresh()
+      width = widthstr:sub(4, 4):byte() * 16777216 + widthstr:sub(3, 3):byte() * 65536 + widthstr:sub(2, 2):byte() * 256 + widthstr:sub(1, 1):byte()
+      height = heightstr:sub(4, 4):byte() * 16777216 + heightstr:sub(3, 3):byte() * 65536 + heightstr:sub(2, 2):byte() * 256 + heightstr:sub(1, 1):byte()
+      return width, height
+    end
+    -- Detect if JPG/JPEG
+    file:seek("set")
+    if file:read(2) == "\255\216" then
+      --[[
+			The strategy is
+			1. Find necessary markers
+			2. Store biggest value in variable
+			3. Return biggest value
+		]]
+      local lastb, curb = 0, 0
+      local xylist = {}
+      local sstr = file:read(1)
+      while sstr ~= nil do
+        lastb = curb
+        curb = sstr:byte()
+        if (curb == 194 or curb == 192) and lastb == 255 then
+          file:seek("cur", 3)
+          local sizestr = file:read(4)
+          local h = sizestr:sub(1, 1):byte() * 256 + sizestr:sub(2, 2):byte()
+          local w = sizestr:sub(3, 3):byte() * 256 + sizestr:sub(4, 4):byte()
+          if w > width and h > height then
+            width = w
+            height = h
+          end
+        end
+        sstr = file:read(1)
+      end
+      if width > 0 and height > 0 then
+        refresh()
+        return width, height
+      end
+    end
+    file:seek("set")
+    -- Detect if GIF
+    if file:read(4) == "GIF8" then
+      --[[
+			The strategy is
+			1. Seek to 0x06 position
+			2. Extract value in little-endian order
+		]]
+      file:seek("set", 6)
+      width, height = file:read(1):byte() + file:read(1):byte() * 256, file:read(1):byte() + file:read(1):byte() * 256
+      refresh()
+      return width, height
+    end
+    -- More image support
+    file:seek("set")
+    -- Detect if Photoshop Document
+    if file:read(4) == "8BPS" then
+      --[[
+			The strategy is
+			1. Seek to position 0x0E
+			2. Get value in big-endian order
+		]]
+      file:seek("set", 14)
+      local heightstr, widthstr = file:read(4), file:read(4)
+      refresh()
+      width = widthstr:sub(1, 1):byte() * 16777216 + widthstr:sub(2, 2):byte() * 65536 + widthstr:sub(3, 3):byte() * 256 + widthstr:sub(4, 4):byte()
+      height = heightstr:sub(1, 1):byte() * 16777216 + heightstr:sub(2, 2):byte() * 65536 + heightstr:sub(3, 3):byte() * 256 + heightstr:sub(4, 4):byte()
+      return width, height
+    end
+    file:seek("end", - 18)
+    -- Detect if Truevision TGA file
+    if file:read(10) == "TRUEVISION" then
+      --[[
+			The strategy is
+			1. Seek to position 0x0C
+			2. Get image width and height in little-endian order
+		]]
+      file:seek("set", 12)
+      width = file:read(1):byte() + file:read(1):byte() * 256
+      height = file:read(1):byte() + file:read(1):byte() * 256
+      refresh()
+      return width, height
+    end
+    file:seek("set")
+    -- Detect if JPEG XR/Tagged Image File (Format)
+    if file:read(2) == "II" then
+      -- It would slow, tell me how to get it faster
+      --[[
+			The strategy is
+			1. Read all file contents
+			2. Find "Btomlong" and "Rghtlong" string
+			3. Extract values in big-endian order(strangely, II stands for Intel byte ordering(little-endian) but it's in big-endian)
+		]]
+      temp = file:read("*a")
+      btomlong = {temp:find("Btomlong")}
+      rghtlong = {temp:find("Rghtlong")}
+      if #btomlong == 2 and #rghtlong == 2 then
+        heightstr = temp:sub(btomlong[2] + 1, btomlong[2] + 5)
+        widthstr = temp:sub(rghtlong[2] + 1, rghtlong[2] + 5)
+        refresh()
+        width = widthstr:sub(1, 1):byte() * 16777216 + widthstr:sub(2, 2):byte() * 65536 + widthstr:sub(3, 3):byte() * 256 + widthstr:sub(4, 4):byte()
+        height = heightstr:sub(1, 1):byte() * 16777216 + heightstr:sub(2, 2):byte() * 65536 + heightstr:sub(3, 3):byte() * 256 + heightstr:sub(4, 4):byte()
+        return width, height
+      end
+    end
+    -- Video support
+    file:seek("set", 4)
+    -- Detect if MP4
+    if file:read(7) == "ftypmp4" then
+      --[[
+			The strategy is
+			1. Seek to 0xFB
+			2. Get value in big-endian order
+		]]
+      file:seek("set", 0xFB)
+      local widthstr, heightstr = file:read(4), file:read(4)
+      refresh()
+      width = widthstr:sub(1, 1):byte() * 16777216 + widthstr:sub(2, 2):byte() * 65536 + widthstr:sub(3, 3):byte() * 256 + widthstr:sub(4, 4):byte()
+      height = heightstr:sub(1, 1):byte() * 16777216 + heightstr:sub(2, 2):byte() * 65536 + heightstr:sub(3, 3):byte() * 256 + heightstr:sub(4, 4):byte()
+      return width, height
+    end
+    file:seek("set", 8)
+    -- Detect if AVI
+    if file:read(3) == "AVI" then
+      file:seek("set", 0x40)
+      width = file:read(1):byte() + file:read(1):byte() * 256 + file:read(1):byte() * 65536 + file:read(1):byte() * 16777216
+      height = file:read(1):byte() + file:read(1):byte() * 256 + file:read(1):byte() * 65536 + file:read(1):byte() * 16777216
+      refresh()
+      return width, height
+    end
+    refresh()
+    return nil
   end
 end
 
